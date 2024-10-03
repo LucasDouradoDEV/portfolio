94df440799b4c4bfe29788f59c8eab0bca711f65
Lucas Dourado PC <lucasdourado28061998@gmail.com>
2024-10-02 23:46:45 -0300

    #6.1 rollback

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { routes } from './app.routes';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    BrowserModule
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
